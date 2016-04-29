c03c74f49a8ed712b420723fbea998da36402299
Joel Nieman <niemanjoel@gmail.com>
2016-04-28 22:01:41 -0400

    initial project and YouTube model set-up

---
+++ StateChamps/app/src/main/java/app/com/joel/statechamps/Model/YouTube/SCVideo.java
@@ -0,0 +1,53 @@
+package app.com.joel.statechamps.Model.YouTube;
+
+/**
+ * Created by Joel on 4/28/16.
+ */
+public class SCVideo {
+
+    private String title;
+    private String description;
+    private String publishedDate;
+    private String videoID;
+    private String thumbnailURL;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getVideoID() {
+        return videoID;
+    }
+
+    public void setVideoID(String videoID) {
+        this.videoID = videoID;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+}
---
+++ StateChamps/app/src/main/java/app/com/joel/statechamps/Model/YouTube/YouTubeAPIInterface.java
@@ -0,0 +1,11 @@
+package app.com.joel.statechamps.Model.YouTube;
+
+/**
+ * Created by Joel on 4/28/16.
+ */
+public class YouTubeAPIInterface {
+
+
+}
+
+
